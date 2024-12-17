2c560e80933b2ccf27622062dd3dc2cc7c77ea08
VotreNom <quentin.vibien@etu.unilim.fr>
2024-12-17 23:09:23 +0100

    test portable

---
+++ src/components/AssignmentDisplay.jsx
@@ -0,0 +1,14 @@
+
+// Il prend en props le tableau d'assignments
+export function AssignmentDisplay({ assignments }) {
+  return (
+    <ul className="space-y-2">
+      {assignments.map((assignment, index) => (
+        <li key={index}>
+          <span>{assignment.giver}</span> offre un beau cadeau à{" "}
+          <span>{assignment.receiver}</span>
+        </li>
+      ))}
+    </ul>
+  );
+}
