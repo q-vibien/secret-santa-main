c18163d665719dc663a75adce9d9ae7ff3e5062d
VotreNom <quentin.vibien@etu.unilim.fr>
2024-12-17 23:12:02 +0100

    test portable

--- src/components/WelcomeScreen.jsx
+++ src/components/WelcomeScreen.jsx
@@ -10,7 +10,7 @@ export function WelcomeScreen({ onStart }) {
         échange de cadeaux entre amis ou collègues.
       </p>
       <button onClick={onStart} className="button text-lg px-8 py-3">
-        Commencer !
+        Commencer 
       </button>
     </div>
   );
